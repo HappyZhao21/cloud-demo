50eed92ab63b7e9232327b6e617f7852e7038aa5
zxq <2671883469@qq.com>
2020-04-12 15:29:29 +0800

    1.增加订单服务

---
+++ xq03-orderservice/src/main/java/xq/example/xq03orderservice/Xq03OrderserviceApplication.java
@@ -0,0 +1,13 @@
+package xq.example.xq03orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xq03OrderserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Xq03OrderserviceApplication.class, args);
+    }
+
+}
---
+++ xq03-orderservice/src/test/java/xq/example/xq03orderservice/Xq03OrderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package xq.example.xq03orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xq03OrderserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
