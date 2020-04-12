db0523e532f79d56cafe07fb7e3ad67c4b10b5e4
zxq <2671883469@qq.com>
2020-04-12 13:46:59 +0800

    1.第一次提交

---
+++ xq01/src/main/java/zhao/cloud/xq01/Xq01Application.java
@@ -0,0 +1,13 @@
+package zhao.cloud.xq01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xq01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Xq01Application.class, args);
+    }
+
+}
---
+++ xq01/src/test/java/zhao/cloud/xq01/Xq01ApplicationTests.java
@@ -0,0 +1,13 @@
+package zhao.cloud.xq01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xq01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
