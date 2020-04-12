e9d4489ab0b9306293a486cc3523794e1e67fbca
zxq <2671883469@qq.com>
2020-04-12 18:53:24 +0800

    1.增加hystrix服务

---
+++ xq07-hystrix-dashboard/src/test/java/xq/example/xq07hystrixdashboard/Xq07HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package xq.example.xq07hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xq07HystrixDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
